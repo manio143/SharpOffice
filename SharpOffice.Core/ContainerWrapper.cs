6b1c906169c1c7d49fa45715c673e2f488dc1c00
manio143 <marian.dziubiak@gmail.com>
2016-03-10 20:43:41 +0100

    [Core] Added ContainerWrapper

---
+++ SharpOffice.Core/ContainerWrapper.cs
@@ -0,0 +1,21 @@
+﻿namespace SharpOffice.Core
+{
+    public static class ContainerWrapper
+    {
+        private static DryIoc.Container _container;
+        public static void Initialize()
+        {
+            _container = new DryIoc.Container();
+        }
+
+        public static DryIoc.Container GetContainer()
+        {
+            return _container;
+        }
+
+        public static void Dispose()
+        {
+            _container.Dispose();
+        }
+    }
+}
