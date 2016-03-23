8f801eea83c5c381613439a1b6118958c41759f8
manio143 <marian.dziubiak@gmail.com>
2016-03-23 11:39:28 +0100

    [Note] Added MainWindowConfiguration.

---
+++ SharpOffice.SharpNote/Configuration/MainWindowConfiguration.cs
@@ -0,0 +1,12 @@
+﻿using SharpOffice.Common.Configuration;
+using SharpOffice.Core.Configuration;
+
+namespace SharpOffice.SharpNote.Configuration
+{
+    [ConfigurationSource("sharpnote-mainwindow.config")]
+    [ConfigurationFormat(typeof(YamlConfigurationFormat))]
+    public class MainWindowConfiguration : PropertyBasedConfiguration
+    {
+
+    }
+}
