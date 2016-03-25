8e0335f8362d0345f27b16b27a77166a4a8d6254
manio143 <marian.dziubiak@gmail.com>
2016-03-25 22:25:53 +0100

    [Note] Added a simple menu.

--- SharpOffice.SharpNote/MainWindow.cs
+++ SharpOffice.SharpNote/MainWindow.cs
@@ -1,9 +1,12 @@
-﻿using Xwt;
+﻿using SharpOffice.Window;
+using Xwt;
 
 namespace SharpOffice.SharpNote
 {
     public class MainWindow : Xwt.Window
     {
+        private Menu _menu;
+
         public MainWindow()
         {
             //DEMO
@@ -11,6 +14,28 @@ namespace SharpOffice.SharpNote
             Width = 500;
             Height = 400;
             Closed += (sender, args) => Xwt.Application.Exit();
+
+            InitializeMenu();
+        }
+
+        private void InitializeMenu()
+        {
+            _menu = new Menu();
+
+            var menuFile = MenuHelper.CreateMenuItem("_File", subMenu: new[]
+            {
+             MenuHelper.CreateMenuItem("_New", subMenu: new []
+             {
+                 MenuHelper.CreateMenuItem("_Source file", command: null),
+                 MenuHelper.CreateMenuItem("_Note", command: null)
+             }),
+             MenuHelper.CreateMenuItem("_Open", command: null),
+             MenuHelper.CreateMenuItem("E_xit", command: (sender, args) => this.Close())
+            });
+
+            _menu.Items.Add(menuFile);
+
+            this.MainMenu = _menu;
         }
     }
 }
