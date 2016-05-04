43c965d4d7365a04338d65ae5548bee56ae8a5e5
manio143 <marian.dziubiak@gmail.com>
2016-05-04 18:34:18 +0200

    [Common] Created IMenuProvider implementation.

---
+++ SharpOffice.Common/Window/Menu/MenuProvider.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using SharpOffice.Core.Window;
+
+namespace SharpOffice.Common.Window.Menu
+{
+    public class MenuProvider : IMenuProvider
+    {
+        public IEnumerable<IMenuElement> TopLevelMenus { get { return _menu.Values; } }
+        private readonly Dictionary<Type, IMenuElement> _menu = new Dictionary<Type, IMenuElement>();
+
+        public T GetOrCreateMenu<T>() where T : IMenuElement, new()
+        {
+            if (!_menu.ContainsKey(typeof (T)))
+                _menu[typeof (T)] = new T();
+            return (T)_menu[typeof (T)];
+        }
+    }
+}
